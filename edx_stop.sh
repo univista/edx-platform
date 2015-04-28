d781603a0eba75a71244d63543e86be6a2caead2
seongho <zudain@naver.com>
2015-04-28 13:57:29 +0900

    modified to edx compile shell

--- edx_stop.sh
+++ edx_stop.sh
@@ -3,5 +3,5 @@ service supervisor stop
 service supervisor.devpi stop
 pkill -u www-data
 rm -rf /edx/var/edxapp/staticfiles/*
-./compile0.sh
+sudo -H -u edxapp bash -c "./compile.sh"
 ./edx_start.sh
