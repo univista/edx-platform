6ee97f14d32e9eff0c0f776bbc3d4ea282062767
seongho <zudain@naver.com>
2015-06-08 04:42:06 +0900

    courses request search query

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -115,8 +115,25 @@ def user_groups(user):
     return group_names
 
 
-@csrf_exempt
+@ensure_csrf_cookie
+@cache_if_anonymous()
 def courses(request):
+    """
+    Render "find courses" page.  The course selection work is done in courseware.courses.
+    """
+    courses = get_courses(request, request.META.get('HTTP_HOST'))
+
+    if microsite.get_value("ENABLE_COURSE_SORTING_BY_START_DATE",
+                           settings.FEATURES["ENABLE_COURSE_SORTING_BY_START_DATE"]):
+        courses = sort_by_start_date(courses)
+    else:
+        courses = sort_by_announcement(courses)
+
+    return render_to_response("courseware/courses.html", {'courses': courses})
+
+@ensure_csrf_cookie
+@cache_if_anonymous()
+def courses_search(request):
     """
     Render "find courses" page.  The course selection work is done in courseware.courses.
     """
