e80e09c5bf8b855132f14ccb2595b91f4f7f861e
seongho <zudain@naver.com>
2015-06-08 04:52:14 +0900

    courses request search query

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -131,12 +131,12 @@ def courses(request):
 
     return render_to_response("courseware/courses.html", {'courses': courses})
 
-@csrf_exempt
+
 def courses_search(request):
     """
     Render "find courses" page.  The course selection work is done in courseware.courses.
     """
-    courses = get_courses(request, request.META.get('HTTP_HOST'))
+    courses = get_courses(csrf(request), request.META.get('HTTP_HOST'))
 
     if microsite.get_value("ENABLE_COURSE_SORTING_BY_START_DATE",
                            settings.FEATURES["ENABLE_COURSE_SORTING_BY_START_DATE"]):
