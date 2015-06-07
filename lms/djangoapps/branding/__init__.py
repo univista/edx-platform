c1a7c5c6de09194908f5e7017db3ae9fc2913612
seongho <zudain@naver.com>
2015-06-08 03:45:56 +0900

    study weeks

--- lms/djangoapps/branding/__init__.py
+++ lms/djangoapps/branding/__init__.py
@@ -11,7 +11,7 @@ def get_visible_courses():
     Return the set of CourseDescriptors that should be visible in this branded instance
     """
 
-    filtered_by_org = microsite.get_value('course_org_filter')
+    filtered_by_org = "edX"
 
     _courses = modulestore().get_courses(org=filtered_by_org)
 
