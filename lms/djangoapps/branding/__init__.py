4ea90cec06bab414cf6acfa2b67583c5798fc52d
seongho <zudain@naver.com>
2015-06-09 02:13:59 +0900

    courses 검색

--- lms/djangoapps/branding/__init__.py
+++ lms/djangoapps/branding/__init__.py
@@ -11,10 +11,8 @@ def get_visible_courses(request):
     Return the set of CourseDescriptors that should be visible in this branded instance
     """
 
-    if request.method == 'POST':
-        filtered_by_org = request.POST['search_query']
-    else:
-        filtered_by_org = microsite.get_value('course_org_filter')
+    filtered_by_org = microsite.get_value('course_org_filter')
+
     _courses = modulestore().get_courses(org=filtered_by_org)
 
     courses = [c for c in _courses
