aadcc812741708a2fe2ed63b57d41b293b9f1b98
seongho <zudain@naver.com>
2015-06-08 18:07:25 +0900

    학습기간

--- common/lib/xmodule/xmodule/course_module.py
+++ common/lib/xmodule/xmodule/course_module.py
@@ -1321,7 +1321,8 @@ class CourseDescriptor(CourseFields, SequenceDescriptor):
         end_week = strftime(self.end, '%W')
         start_week = strftime(self.start, '%W')
 
-        return int(end_week) - int(start_week)
+        #return int(end_week) - int(start_week)
+        return self.end
 
 
     def start_datetime_text(self, format_string="SHORT_DATE"):
