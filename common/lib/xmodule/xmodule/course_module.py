e84abcfd6d236250880a48032f6476567116b097
seongho <zudain@naver.com>
2015-06-08 17:10:42 +0900

    학습기간

--- common/lib/xmodule/xmodule/course_module.py
+++ common/lib/xmodule/xmodule/course_module.py
@@ -1283,41 +1283,7 @@ class CourseDescriptor(CourseFields, SequenceDescriptor):
         return self.location.course_key
 
     def enrollment_start_datetime_text(self, format_string="SHORT_DATE"):
-        """
-        Returns the desired text corresponding the course's start date and time in UTC.  Prefers .advertised_start,
-        then falls back to .start
-        """
-        i18n = self.runtime.service(self, "i18n")
-        _ = i18n.ugettext
-        strftime = i18n.strftime
-
-        def try_parse_iso_8601(text):
-            try:
-                result = Date().from_json(text)
-                if result is None:
-                    result = text.title()
-                else:
-                    result = strftime(result, format_string)
-                    if format_string == "DATE_TIME":
-                        result = self._add_timezone_string(result)
-            except ValueError:
-                result = text.title()
-
-            return result
-
-        if isinstance(self.enrollment_start, basestring):
-            return try_parse_iso_8601(self.enrollment_start)
-        elif self.start_date_is_still_default:
-            # Translators: TBD stands for 'To Be Determined' and is used when a course
-            # does not yet have an announced start date.
-            return _('TBD')
-        else:
-            when = self.enrollment_start
-
-            if format_string == "DATE_TIME":
-                return self._add_timezone_string(strftime(when, format_string))
-
-            return strftime(when, format_string)
+        return self.enrollment_start
 
     def enrollment_end_datetime_text(self, format_string="SHORT_DATE"):
         """
