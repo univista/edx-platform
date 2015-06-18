6a70dd9900c9b1cbb42efdba770254085b0f7ee4
seongho <zudain@naver.com>
2015-06-19 01:11:04 +0900

    검색

--- lms/djangoapps/branding/__init__.py
+++ lms/djangoapps/branding/__init__.py
@@ -49,7 +49,7 @@ def get_visible_courses_search(request):
     """
 
     #_courses = modulestore().get_courses(name=filtered_by_org)
-    _courses = modulestore().get_courses(course=request.POST['search_query'])
+    _courses = modulestore().get_courses(name=request.POST['search_query'])
 
     courses = [c for c in _courses
                if isinstance(c, CourseDescriptor)]
